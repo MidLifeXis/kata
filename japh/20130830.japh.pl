2009ef79eaa29c1db739be79cb6eaca2b204e644
Brian T. Wightman <brian.t.wightman@jci.com>
2013-08-30 09:05:00 -0500

    Removal of whitespace

--- japh/20130830.japh.pl
+++ japh/20130830.japh.pl
@@ -1,29 +1,10 @@
 # Cutting down a tree
-use strict;
-use warnings;
-my $sapling = tree->plant;
-my $tree = $sapling->grow;
-$tree->fall;
-sub tree::plant {
-    my @nodes = map{ x($_) } split('','Just another Perl hacker,');
-    bless { nodes => \@nodes }, $_[0];
-}
-sub tree::grow {
-    $_[0]->{trunk} = $_[0]->branch( 0, $#{$_[0]->{nodes}} );
-    $_[0]->{nodes} = undef;
-    $_[0]
-}
-sub tree::branch {
-    my ( $self, $start, $stop ) = @_;
-    my $mid = int( ( $start + $stop ) / 2 );
-    my $fork  = $self->{nodes}[$mid];
-    my $left  = $self->branch( $start,   $mid - 1 ) if $start < $mid;
-    my $right = $self->branch( $mid + 1, $stop  )   if $mid   < $stop;
-    bless [ $left, $fork, $right ], 'branch';
-}
-sub tree::fall {
-    $_[0]->{trunk} = undef;
-}
+$sapling=tree->plant;$tree=$sapling->grow;$tree->fall;sub tree::plant{@n=map{x(
+$_)}split('','Just another Perl hacker,');bless{n=>\@n},$_[0]}sub tree::grow{$_[
+0]->{trunk}=$_[0]->branch(0,$#{$_[0]->{n}});$_[0]->{n}=undef;$_[0]}sub
+tree::branch{($a,$b,$e)=@_;$m=int(($b+$e)/2);$f=$_[0]->{n}[$m];$l=$_[0]->branch(
+$b,$m-1)if$b<$m;$r=$_[0]->branch($m+1,$e)if$m<$e;bless [$l,$f,$r],'branch'}
+sub tree::fall{$_[0]->{trunk}=1}
 sub x{ bless \$_[0],"x"}
 sub x::DESTROY{print ${$_[0]}}
-sub branch::DESTROY{$_[0]->[$_] = 1 for (0..2)}
+sub branch::DESTROY{$_[0]->[$_]=1 for(0..2)}
