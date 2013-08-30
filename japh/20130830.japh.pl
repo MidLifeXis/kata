dddc2757f050cf10e97840921c6c23483f345420
Brian T. Wightman <brian.t.wightman@jci.com>
2013-08-30 09:10:31 -0500

    More substitutions

--- japh/20130830.japh.pl
+++ japh/20130830.japh.pl
@@ -1,7 +1,7 @@
 # Cutting down a tree
-$t=t->plant;$t->grow;$t->fall;sub t::plant{@n=map{x($_)}split('','Just another'.
-' Perl hacker,');bless{n=>\@n},$_[0]}sub t::grow{$_[0]->{trunk}=$_[0]->branch(0,
-$#{$_[0]->{n}});$_[0]->{n}=undef;$_[0]}sub t::branch{($a,$b,$e)=@_;$m=int(($b+$e
-)/2);$f=$_[0]->{n}[$m];$l=$_[0]->branch($b,$m-1)if$b<$m;$r=$_[0]->branch($m+1,$e
-)if$m<$e;bless [$l,$f,$r],'branch'}sub t::fall{$_[0]->{trunk}=1}sub x{bless \$_[
-0],"x"}sub x::DESTROY{print${$_[0]}}sub branch::DESTROY{$_[0]->[$_]=1 for(0..2)}
+$t=t->p;$t->g;$t->f;sub t::p{@n=map{x($_)}split('','Just another'.
+' Perl hacker,');bless{n=>\@n},$_[0]}sub t::g{$_[0]->{t}=$_[0]->f(0,
+$#{$_[0]->{n}});$_[0]->{n}=1;$_[0]}sub t::f{($a,$b,$e)=@_;$m=int(($b+$e
+)/2);$f=$_[0]->{n}[$m];$l=$_[0]->f($b,$m-1)if$b<$m;$r=$_[0]->f($m+1,$e
+)if$m<$e;bless [$l,$f,$r],'f'}sub t::f{$_[0]->{t}=1}sub x{bless \$_[
+0],"x"}sub x::DESTROY{print${$_[0]}}sub f::DESTROY{$_[0]->[$_]=1 for(0..2)}
