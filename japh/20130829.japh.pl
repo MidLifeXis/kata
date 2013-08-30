0b3900ce4b5229fb7d37b67fce74718fc441f275
Brian T. Wightman <brian.t.wightman@jci.com>
2013-08-30 08:51:57 -0500

    Mark the PerlMonks node containing this JAPH

--- japh/20130829.japh.pl
+++ japh/20130829.japh.pl
@@ -1,4 +1,4 @@
-# Destructive JAPH
+# Destructive JAPH - http://www.perlmonks.org/?node_id=1051452
 @x = map{ x($_) } split('','Just another Perl hacker,');
 shift(@x) while @x;
 sub x{ bless \$_[0],"x"}
