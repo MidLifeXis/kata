d85214e0b976dbae54af55da1833a5fe7a1677d0
Brian T. Wightman <brian.t.wightman@jci.com>
2014-03-04 13:34:27 -0600

    Add some japh samples

---
+++ japh/choroba/choroba.1052185.a.pl
@@ -0,0 +1,3 @@
+sub e{sub _{-72} er}eval($/='sub J{map{substr$/,ref('.q:)?pop@$_\:$_,$$_[0]||1}
+@_}sub f{uc join "system",J(@_)}print J 4=>1,[2,13],-1,010-1,[ _ ],42-020,hex"e
+";print+h,e,$",f(__LINE__*2+2),e,qw,l,,J(-1,-hex 20),q{ack},e,J hex ord(0)/4 :)
---
+++ japh/choroba/choroba.1052185.b.pl
@@ -0,0 +1,3 @@
+sub k{join shift,@_}sub e{sub _{-60}er}eval($/='sub J{map{substr$/,ref()?pop'.q
+:@$_\:$_,$$_[0]||1}@_}sub f{uc k"system",J(@_)}print J 4,1,[2,13],-1,010-1,[_],
+42 -020,hex e;print+h,e,$",f(__LINE__+6),e,qw,l,,J(-1,-hex 16),q{ack},e,J 18 :)
