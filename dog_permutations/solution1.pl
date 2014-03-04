44f2a8d46647698b88936bfdf22232e54a665d05
Brian T. Wightman <brian.t.wightman@jci.com>
2014-03-04 13:33:22 -0600

    Update dog list and alphabet

--- dog_permutations/solution1.pl
+++ dog_permutations/solution1.pl
@@ -1,39 +1,37 @@
 use strict;
 use warnings;
 
-my @dogs =
+# my $alphabet = 'A(1),D(2),F(1),I(1),O(1),R(1),U(1)';
+# my @dogs     = qw( dorf TOYOTA HONDA AUDI FORD );
 
-    qw( TOYOTA HONDA AUDI FORD );
+my @dogs = qw(
 
-# qw(
+Affenpinscher AfghanHound AiredaleTerrier Akita AlaskanMalamute
+AmericanEnglishCoonhound AmericanEskimoDog AmericanFoxhound Basenji
+BassetHound Beagle BeardedCollie Beauceron BlackandTanCoonhound
+Bloodhound BluetickCoonhound BorderCollie BorderTerrier Borzoi
+BostonTerrier BouvierdesFlandres Boxer BoykinSpaniel Briard Brittany
+BrusselsGriffon BullTerrier Bulldog Bullmastiff ChesapeakeBayRetriever
+Chihuahua ChineseCrested ChineseShar-Pei Chinook ChowChow
+ClumberSpaniel CockerSpaniel Collie Curly-CoatedRetriever Dachshund
+Dalmatian DandieDinmontTerrier DobermanPinscher DoguedeBordeaux
+EnglishCockerSpaniel EnglishFoxhound EnglishSetter
+EnglishSpringerSpaniel GermanPinscher GermanShepherdDog
+GermanShorthairedPointer GermanWirehairedPointer GiantSchnauzer
+GlenofImaalTerrier GoldenRetriever GordonSetter GreatDane Greyhound
+Harrier IbizanHound IcelandicSheepdog IrishSetter IrishTerrier Kuvasz
+LabradorRetriever LakelandTerrier LhasaApso Lowchen Maltese
+ManchesterTerrier Mastiff MiniatureBullTerrier MiniaturePinscher
+MiniatureSchnauzer NeapolitanMastiff Newfoundland NorfolkTerrier
+Pekingese PembrokeWelshCorgi PharaohHound Plott PortugueseWaterDog Pug
+Puli PyreneanShepherd RatTerrier ShibaInu ShihTzu SiberianHusky
+SilkyTerrier SkyeTerrier SmoothFoxTerrier SoftCoatedWheatenTerrier
+SpinoneItaliano St.Bernard StaffordshireBullTerrier WireFoxTerrier
+WirehairedPointingGriffon Xoloitzcuintli YorkshireTerrier
 
-# Affenpinscher AfghanHound AiredaleTerrier Akita AlaskanMalamute
-# AmericanEnglishCoonhound AmericanEskimoDog AmericanFoxhound Basenji
-# BassetHound Beagle BeardedCollie Beauceron BlackandTanCoonhound
-# Bloodhound BluetickCoonhound BorderCollie BorderTerrier Borzoi
-# BostonTerrier BouvierdesFlandres Boxer BoykinSpaniel Briard Brittany
-# BrusselsGriffon BullTerrier Bulldog Bullmastiff ChesapeakeBayRetriever
-# Chihuahua ChineseCrested ChineseShar-Pei Chinook ChowChow
-# ClumberSpaniel CockerSpaniel Collie Curly-CoatedRetriever Dachshund
-# Dalmatian DandieDinmontTerrier DobermanPinscher DoguedeBordeaux
-# EnglishCockerSpaniel EnglishFoxhound EnglishSetter
-# EnglishSpringerSpaniel GermanPinscher GermanShepherdDog
-# GermanShorthairedPointer GermanWirehairedPointer GiantSchnauzer
-# GlenofImaalTerrier GoldenRetriever GordonSetter GreatDane Greyhound
-# Harrier IbizanHound IcelandicSheepdog IrishSetter IrishTerrier Kuvasz
-# LabradorRetriever LakelandTerrier LhasaApso Lowchen Maltese
-# ManchesterTerrier Mastiff MiniatureBullTerrier MiniaturePinscher
-# MiniatureSchnauzer NeapolitanMastiff Newfoundland NorfolkTerrier
-# Pekingese PembrokeWelshCorgi PharaohHound Plott PortugueseWaterDog Pug
-# Puli PyreneanShepherd RatTerrier ShibaInu ShihTzu SiberianHusky
-# SilkyTerrier SkyeTerrier SmoothFoxTerrier SoftCoatedWheatenTerrier
-# SpinoneItaliano St.Bernard StaffordshireBullTerrier WireFoxTerrier
-# WirehairedPointingGriffon Xoloitzcuintli YorkshireTerrier
+);
 
-# );
-
-#my $alphabet = 'A(8),B(2),C(5),8(D),7(E),F(1),G(3),H(9),I(8),K(1),L(9),M(1),N(12),O(13),P(1),Q(0),R(5),S(6),T(4),U(6),V(0),W(1),X(1),Y(0),Z(0)';
-my $alphabet = 'A(1),D(2),F(1),I(1),O(1),R(1),U(1)';
+my $alphabet = 'A(8),B(2),C(5),8(D),7(E),F(1),G(3),H(9),I(8),J(0),K(1),L(9),M(1),N(12),O(13),P(1),Q(0),R(5),S(6),T(4),U(6),V(0),W(1),X(1),Y(0),Z(1)';
 
 my @alphabet = (0) x 26;
 while ($alphabet =~ m/([A-Z])\((\d+)\)/g) {
